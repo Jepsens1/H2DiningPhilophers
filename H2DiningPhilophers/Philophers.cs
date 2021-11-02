746e5b5ff3421761abf1deb7b7aa5bfec92e38c4
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2021-11-02 13:32:17 +0100

    Add project files.

---
+++ H2DiningPhilophers/Philophers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace H2DiningPhilophers
+{
+    class Philophers
+    {
+        bool[] forks;
+        public int leftfork { get; set; }
+        int number;
+        public int rightfork { get; set; }
+        public string status { get; set; } = "Thinking";
+
+        public Philophers(bool[] forks, int number)
+        {
+            this.forks = forks;
+            this.number = number;
+            leftfork = this.number;
+            rightfork = (this.number + 1) % 5;
+        }
+        public void Start()
+        {
+            //Creating random, so its random how long a philosphers will wait and how long he will eat
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+            while (true)
+            {
+                Thread.Sleep(r.Next(1000, 5000));
+                //Changes status
+                status = "Waiting";
+                //Locks
+                Monitor.Enter(forks);
+                try
+                {
+                    while (forks[rightfork] == true || forks[leftfork] == true)
+                    {
+                        Monitor.Wait(forks);
+                    }
+                    //Sets right and left fork to true, which means its in use, and changes status and uses pulseall, to let every thread know
+                    forks[rightfork] = true;
+                    forks[leftfork] = true;
+                    status = "Eating";
+                    Monitor.PulseAll(forks);
+
+                }
+                finally
+                {
+                    Monitor.Exit(forks);
+                }
+
+                //Sleeps to simulate eating
+                Thread.Sleep(r.Next(1000, 5000));
+
+                //Locks
+                Monitor.Enter(forks);
+                try
+                {
+                    if (forks[rightfork] == false || forks[leftfork] == false)
+                    {
+                        Debug.WriteLine("Something went wrong");
+                    }
+                    forks[rightfork] = false;
+                    forks[leftfork] = false;
+                    Monitor.PulseAll(forks);
+                }
+                finally
+                {
+                    Monitor.Exit(forks);
+                }
+                status = "Thinking";
+            }
+        }
+    }
+}
---
+++ H2DiningPhilophers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace H2DiningPhilophers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //each element indicates if a fork is in use (true)
+            bool[] forks = new bool[5];
+            Philophers[] philophers = new Philophers[5];
+            //Creating 5 threads for each Philospher
+            for (int i = 0; i < philophers.Length; i++)
+            {
+                Philophers philosof = new Philophers(forks, i);
+                philophers[i] = philosof;
+                Thread Philosofthread = new Thread(philosof.Start);
+                Philosofthread.Start();
+            }
+            while (true)
+            {
+                //Displays the status of each philophers
+                Console.WriteLine($"Philophers 1: {philophers[0].status}\nPhilophers 2: {philophers[1].status}\nPhilophers 3: " +
+                    $"{philophers[2].status}\nPhilophers 4: {philophers[3].status}\nPhilophers 5: {philophers[4].status}\n");
+                Thread.Sleep(2000);
+                Console.Clear();
+            }
+
+        }
+    }
+}
